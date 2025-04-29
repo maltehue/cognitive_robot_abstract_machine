1b45d6a2ea61f663d76928a5b9620d10f3fbdb56
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-04-29 04:04:50 +0200

    [RDR] prompt user to add extra rules.

--- test_rdr_world.py
+++ test_rdr_world.py
@@ -13,12 +13,15 @@ from ripple_down_rules.utils import is_matching
 
 @dataclass
 class WorldEntity:
-    world: Optional[World] = field(kw_only=True, default=None, repr=False)
+    world: World = field(kw_only=True, repr=False)
 
 
 @dataclass
 class Body(WorldEntity):
     name: str
+
+    def __hash__(self):
+        return hash(self.name)
 
 
 @dataclass
@@ -51,11 +54,14 @@ class PrismaticConnection(Connection):
 class World:
     bodies: List[Body] = field(default_factory=list)
     connections: List[Connection] = field(default_factory=list)
+    views: List[View] = field(default_factory=list, repr=False)
 
 
 @dataclass
 class View(WorldEntity):
-    ...
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.world.views.append(self)
 
 
 @dataclass
@@ -64,13 +70,27 @@ class Drawer(View):
     container: Container
     correct: Optional[bool] = None
 
+    def __hash__(self):
+        return hash((self.handle.name, self.container.name))
+
+
+@dataclass
+class Cabinet(View):
+    container: Container
+    drawers: List[Drawer] = field(default_factory=list)
+
+    def __hash__(self):
+        return hash(tuple([self.container.name] + [hash(drawer) for drawer in self.drawers]))
+
 
 class TestRDRWorld(TestCase):
     drawer_case_queries: List[CaseQuery]
+    world: World
 
     @classmethod
     def setUpClass(cls):
         world = World()
+        cls.world = world
 
         handle = Handle('h1', world=world)
         handle_2 = Handle('h2', world=world)
@@ -82,17 +102,39 @@ class TestRDRWorld(TestCase):
         world.bodies = [handle, container_1, container_2, handle_2]
         world.connections = [connection_1, connection_2]
 
-        all_views = []
-
-        i = 1
+        all_possible_drawers = []
         for handle in [body for body in world.bodies if isinstance(body, Handle)]:
             for container in [body for body in world.bodies if isinstance(body, Container)]:
                 view = Drawer(handle, container, world=world)
-                all_views.append(view)
-                i += 1
+                all_possible_drawers.append(view)
 
-        print(all_views)
-        cls.drawer_case_queries = [CaseQuery(view, "correct", bool, True, default_value=False) for view in all_views]
+        print(all_possible_drawers)
+        cls.drawer_case_queries = [CaseQuery(possible_drawer, "correct", bool, True, default_value=False)
+                                   for possible_drawer in all_possible_drawers]
+
+    def test_view_rdr(self):
+        self.get_view_rdr(use_loaded_answers=True, save_answers=False, append=False)
+
+    def get_view_rdr(self, use_loaded_answers: bool = True, save_answers: bool = False,
+                     append: bool = False):
+        expert = Human(use_loaded_answers=use_loaded_answers)
+        filename = os.path.join(os.getcwd(), "test_expert_answers/view_rdr_expert_answers_fit")
+        if use_loaded_answers:
+            expert.load_answers(filename)
+        rdr = GeneralRDR()
+        try:
+            rdr.fit_case(CaseQuery(self.world, "views", View, False), expert=expert,
+                    add_extra_conclusions=True)
+        except Exception as e:
+            if append:
+                expert.use_loaded_answers = False
+                rdr.fit_case(CaseQuery(self.world, "views", View, False), expert=expert,
+                             add_extra_conclusions=True)
+            else:
+                raise e
+        if save_answers:
+            expert.save_answers(filename, append=append)
+        print(rdr.classify(self.world))
 
     def test_drawer_rdr(self):
         self.get_drawer_rdr(use_loaded_answers=True, save_answers=False)
