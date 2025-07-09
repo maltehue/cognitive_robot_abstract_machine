e54c1f5abc42ab8d37fba9b6df7e419a569a0d89
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-07-09 02:01:44 +0200

    [RDR] class hierarchy works.

--- test_tracked_object.py
+++ test_tracked_object.py
@@ -1,11 +1,19 @@
+import os
+from os.path import dirname
+
 import pytest
 
 from ripple_down_rules.rdr import GeneralRDR
-from datasets import Drawer, Handle, Cabinet
+from .datasets import Drawer, Handle, Cabinet
 
+def test_construct_class_hierarchy():
+    Drawer.make_class_dependency_graph()
+    assert len(Drawer._dependency_graph.nodes()) == 16
+    assert len(Drawer._dependency_graph.edges()) == 14
+    Drawer.to_dot(os.path.join(dirname(__file__), "dependency_graph"))
 
 @pytest.mark.skip("Not Implemented yet")
-def test_construct_class_dependency_graph():
+def test_construct_class_composition_and_dependency():
     assert Drawer.has_one(Handle)
     assert Cabinet.has_many(Drawer)
     assert Cabinet.depends_on(Drawer)
