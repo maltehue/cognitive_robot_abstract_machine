6504113fcaacd34a3b695b2635a46409682916e6
Simon Stelter <stelter@uni-bremen.de>
2026-03-17 15:41:53 +0100

    tuned piqp reg_lower_limit

--- giskardpy/src/giskardpy/qp/solvers/qp_solver_piqp.py
+++ giskardpy/src/giskardpy/qp/solvers/qp_solver_piqp.py
@@ -20,6 +20,7 @@ class QPSolverPIQP(QPSolver[QPDataExplicit]):
         self.solver.settings.eps_rel = 1e-7
         self.solver.settings.eps_duality_gap_abs = 1e-4
         self.solver.settings.eps_duality_gap_rel = 1e-5
+        self.solver.settings.reg_lower_limit = 1e-11
         # self.solver.settings.kkt_solver = piqp.KKTSolver.sparse_multistage
 
     def solver_call_explicit_interface(self, qp_data: QPDataExplicit) -> np.ndarray:
